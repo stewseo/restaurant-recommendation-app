06066a18cf9cbf060d02d592f7462cb9a4ee189f
stewseo <sseo3@mail.ccsf.edu>
2022-10-04 03:33:19 -0700

    test indexing all results  from yelp businesses search request

--- src/main/java/com/example/client/yelp_fusion/category/Categories.java
+++ src/main/java/com/example/client/yelp_fusion/category/Categories.java
@@ -1,6 +1,7 @@
 package com.example.client.yelp_fusion.category;
 
 public class Categories {
+    String alias, title;
 
     public Categories() {
 
--- src/test/java/com/example/client/yelp_fusion/business/BusinessSearchRequestWithCategoriesTest.java
+++ src/test/java/com/example/client/yelp_fusion/business/BusinessSearchRequestWithCategoriesTest.java
@@ -25,84 +25,87 @@ class BusinessSearchRequestWithCategoriesTest extends AbstractYelpRequestTestCas
             "steakhouses", "peruvian", "ramen", "udon");
 
     List<String> districts = List.of("mission", "Marina/Cow Hollow", "old oakland", "inner sunset", "richmond", "japantown");
+
     ObjectMapper mapper;
-    String restaurantCategory = restaurantCategories.get(22);
+
 
     @Test
     void prepareRequestWithRestaurantCategoryTest() throws IOException {
-
+        for(String restaurantCategory : restaurantCategories) {
         requestParams.put("categories", restaurantCategory);
 
-        requestParams.forEach((k, v) -> {
-            if (requestUsingCurl.toString().endsWith("search")) {
-                requestUsingCurl.append("?")
-                        .append(k)
-                        .append("=")
-                        .append(v);
+            requestParams.forEach((k, v) -> {
+                if (requestUsingCurl.toString().endsWith("search")) {
+                    requestUsingCurl.append("?")
+                            .append(k)
+                            .append("=")
+                            .append(v);
 
-            } else {
-                requestUsingCurl
-                        .append("&")
-                        .append(k)
-                        .append("=")
-                        .append(v);
+                } else {
+                    requestUsingCurl
+                            .append("&")
+                            .append(k)
+                            .append("=")
+                            .append(v);
+                }
+            }); // search?location=SF&latitude=&longitude=&sort_by=distance&limit=50categories= iterate through list & offset = depends on response parameter: total
+
+            JsonObject jsonObject = performRequest(requestUsingCurl.toString()); // GetBusinessSearchResponse response = this.transport.performRequest(GetBusinessSearchRequest.Builder ...)
+
+            mapper = new ObjectMapper();
+
+            String loc = requestParams.get("location");
+
+            // index results 1-50
+            GetBusinessResponse businessResponse = mapper.readValue(jsonObject.toString(), GetBusinessResponse.class);
+            IndexResponse response = client.index(i -> i // Creates or updates a document in an index.
+                    .index("restaurants") // restaurants-index-000001 alias
+                    .id(String.format("%s-restaurants-%s-results-1", loc, restaurantCategory)) // Document ID
+                    .document(businessResponse) // Request body
+            );
+
+            System.out.println("doc id = " + response.id());
+            System.out.println("index = " + response.index());
+            System.out.println("result = " + response.result());
+            System.out.println("businessResponse.getTotal= " + businessResponse.getTotal());
+
+            int total = jsonObject.getInt("total"); // max results per page = 50
+
+            if (total > 50) {
+                addOffsetParameter(total, restaurantCategory); // iterate results 51 - up to 1000
             }
-        }); // search?location=SF&latitude=&longitude=&sort_by=distance&limit=50categories= iterate through list & offset = depends on response parameter: total
-
-        JsonObject jsonObject = performRequest(requestUsingCurl.toString()); // GetBusinessSearchResponse response = this.transport.performRequest(GetBusinessSearchRequest.Builder ...)
-
-        mapper = new ObjectMapper();
-
-        // index results 1-50
-        GetBusinessResponse businessResponse = mapper.readValue(jsonObject.toString(), GetBusinessResponse.class);
-        IndexResponse response = client.index(i -> i // Creates or updates a document in an index.
-                .index("restaurants-index-000001") // The name of the index
-                .id(String.format("restaurants-%s1", restaurantCategory)) // Document ID
-                .document(businessResponse) // Request body
-        );
-
-        System.out.println("doc id = " + response.id());
-        System.out.println("index = " + response.index());
-        System.out.println("result = " + response.result());
-        System.out.println("businessResponse.getTotal= " + businessResponse.getTotal());
-        // max results per page = 50
-        int total = jsonObject.getInt("total");
-
-        if (total > 50) {
-            addOffsetParameter(total);
         }
     }
 
 
-    private void addOffsetParameter(int total) throws IOException {
-        // add request param for offset
-        requestParams.put("offset", String.valueOf(limit));
-        requestUsingCurl
-                .append("&")
-                .append("offset")
-                .append("=");
+    private void addOffsetParameter(int total, String restaurantCategory) throws IOException {
+            // add request param for offset
+            requestParams.put("offset", String.valueOf(limit));
+            requestUsingCurl
+                    .append("&")
+                    .append("offset")
+                    .append("=");
 
-        int length = requestUsingCurl.toString().length();
+            int length = requestUsingCurl.toString().length();
 
-        // if total results > max results, add category filter
-        // limit = max results per page, increment offset by limit, iterate until
-        for (int offset = limit; offset <= total + 50; offset += limit) {
-            String request = requestUsingCurl.toString() + offset;
+            // if total results > max results, add category filter
+            // limit = max results per page, increment offset by limit, iterate until
+            for (int offset = limit; offset <= total + 50; offset += limit) {
+                String request = requestUsingCurl.toString() + offset;
 
-            JsonObject jsonObj = performRequest(request);
+                JsonObject jsonObj = performRequest(request);
 
-            GetBusinessResponse businessRes = mapper.readValue(jsonObj.toString(), GetBusinessResponse.class);
+                GetBusinessResponse businessRes = mapper.readValue(jsonObj.toString(), GetBusinessResponse.class);
 
-            String docId = String.format("restaurants-%s%s", restaurantCategory, offset);
+                String docId = String.format("restaurants-%s%s", restaurantCategory, offset);
 
-            IndexResponse response = client.index(i -> i
-                    .index("restaurants-index-000001")
-                    .id(docId)
-                    .document(businessRes)
-            );
-            System.out.println("current offset = " + offset);
-        }
-
+                IndexResponse response = client.index(i -> i
+                        .index("restaurants-index-000001")
+                        .id(docId)
+                        .document(businessRes)
+                );
+                System.out.println("current offset = " + offset);
+            }
     }
 
     private JsonObject performRequest(String request) throws IOException {
